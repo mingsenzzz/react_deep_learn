fbba03aa9c57148f89ce0254d3007ee3f6912fec
mingsenzzz <1255678570@qq.com>
2021-07-05 09:23:05 +0800

    add:添加编程式导航的例子

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,22 @@
 import React from "react";
-function Home() {
-  return <div>hgomePage</div>;
+import { Button } from "antd";
+import { Router, withRouter } from "react-router-dom";
+
+function Home(props) {
+  const { history } = props;
+  console.log(props, "props");
+  return (
+    <div>
+      hgomePage
+      <Button
+        onClick={() => {
+          history.push("/users");
+        }}
+      >
+        To users page
+      </Button>
+    </div>
+  );
 }
 
-export default Home;
+export default withRouter(Home);
