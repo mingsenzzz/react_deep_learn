d604595d794b29f02094c70b33a3f3b96e03177e
mingsenzzz <1255678570@qq.com>
2021-07-05 19:35:12 +0800

    change:优化异步的action使用

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,3 +5,22 @@ export const addTodoItem = ({ id, text }) => {
     text,
   };
 };
+
+//异步的action
+export const addOneTaskAction = (next, getstate, text, callback) => {
+  //第一个参数是改造前的dispatch
+  //第二个参数是获取redux中的原状态
+  //第二个参数后面的是自定义的参数
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        type: "ADD_TODO",
+        text,
+        id: Date.now(),
+      });
+    }, 2000);
+  }).then((res) => {
+    next(res);
+    callback && callback();
+  });
+};
