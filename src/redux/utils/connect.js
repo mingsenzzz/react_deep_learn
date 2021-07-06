d77a17ec97646b6f02d39976cdbd9e997dfd0457
mingsenzzz <1255678570@qq.com>
2021-07-06 11:47:43 +0800

    add:完善connect的实现

--- src/redux/utils/connect.js
+++ src/redux/utils/connect.js
@@ -10,6 +10,27 @@ const store = createStore(reducers);
 function connect(mapStateToProps, mapDispatchToProp) {
   return function enHanceHoc(WrappedComponent) {
     return class HocComponent extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          storeContext: mapStateToProps(store.getState()),
+        };
+      }
+
+      componentDidMount() {
+        //订阅store的变化
+        this.unSubscribe = store.subscribe(() => {
+          this.setState({
+            storeContext: store.getState(),
+          });
+        });
+      }
+
+      componentWillUnmount() {
+        //清除订阅
+        this.unSubscribe();
+      }
+
       render() {
         return (
           <WrappedComponent
