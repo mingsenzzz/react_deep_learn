2c84b631666f1b1247045171e81a16851543e580
mingsenzzz <1255678570@qq.com>
2021-07-12 18:28:28 +0800

    add；手写promise，支持可以链式调用

--- src/newKnowledge/myPromise.js
+++ src/newKnowledge/myPromise.js
@@ -12,7 +12,7 @@ class MyPromise {
 
   resolve = (value) => {
     this.value = value;
-    this.status = "";
+    this.status = "fullfilled";
 
     //当状态改变成时，则执行回调函数
     while (this.onResolveCallback.length) {
@@ -31,29 +31,40 @@ class MyPromise {
   };
 
   then = (resolveFun, rejectFun) => {
-    if (this.status == "fullfilled") {
-      resolveFun(this.value);
-    }
-    if (this.status == "rejected") {
-      if (rejectFun) {
-        rejectFun(this.errorMsg);
-      } else {
-        this.catch();
-      }
-    }
+    const promise2 = new MyPromise((resolve, reject) => {
+      if (this.status == "fullfilled") {
+        //获取执行resolve回调函数之后的结果（.then中第一个回调return的值）
+        const x = resolveFun(this.value);
 
-    //因为如果执行器中有异步操作的话，
-    //并且修改状态的resolve和reject在异步操作中，
-    //那么状态不会修改，所以需要存储，.then时传入的两个异步回调；
-    if (this.status == "pending") {
-      this.onResolveCallback.push(resolveFun);
-      this.onRejectCallback.push(rejectFun);
-    }
+        resolvePromise(x, resolve, reject);
+      }
+      if (this.status == "rejected") {
+        rejectFun(this.errorMsg);
+      }
+      //因为如果执行器中有异步操作的话，
+      //并且修改状态的resolve和reject在异步操作中，
+      //那么状态不会修改，所以需要存储，.then时传入的两个异步回调；
+      if (this.status == "pending") {
+        this.onResolveCallback.push(resolveFun);
+        this.onRejectCallback.push(rejectFun);
+      }
+    });
+    return promise2;
   };
 
   catch = (catchFunc) => {
     catchFunc(this.errorMsg);
   };
+}
+
+function resolvePromise(x, resolve, reject) {
+  //判断.then中第一个回调函数的返回值，如果是promise的话，执行它的then
+  if (x instanceof MyPromise) {
+    x.then(resolve, reject);
+  } else {
+    //如果.then 第一个回调中return一个常量，则会返回一个成功的promise
+    resolve(x);
+  }
 }
 
 export default MyPromise;
--- src/newKnowledge/promise.js
+++ src/newKnowledge/promise.js
@@ -18,22 +18,17 @@ const PromiseComponent = () => {
   //相当于先执行执行器，然后又调用了then方法，
   //如果执行器中是异步的操作，那么在调用then方法时，promise的状态还没有改变，会有问题
   const childP = new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("成功啦");
-    }, 1000);
-  });
-
-  childP.then((res) => {
-    console.log(res, "res1111");
-  });
-
-  childP.then((res) => {
-    console.log(res, "res2222");
-  });
-
-  childP.then((res) => {
-    console.log(res, "res3333");
-  });
+    // setTimeout(() => {
+    resolve("成功啦");
+    // }, 1000);
+  })
+    .then((res) => {
+      console.log(res, "res1111");
+      return 2222;
+    })
+    .then((res) => {
+      console.log(res, "res2222");
+    });
 
   return <div>2222222</div>;
 };
